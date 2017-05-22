75c2c986d8341d77b8d745ebc1d7273c92f99ba2
chunkaihuang <atohuang@bu.edu>
2017-05-22 15:24:13 +0800

    Update web.rb

--- web.rb
+++ web.rb
@@ -1,5 +1,4 @@
 require 'sinatra'
-require 'sinatra/logger'
 require 'line/bot'
 
 def client
@@ -18,24 +17,22 @@ post '/callback' do
   end
 
   events = client.parse_events_from(body)
-  events.each do |event|
+  events.each { |event|
     case event
     when Line::Bot::Event::Message
       case event.type
       when Line::Bot::Event::MessageType::Text
-        say_message = event.message['text']
         message = {
           type: 'text',
-          text: "回應: #{say_message}"
+          text: event.message['text']
         }
-        response = client.reply_message(event['replyToken'], message)
+        client.reply_message(event['replyToken'], message)
       when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
         response = client.get_message_content(event.message['id'])
         tf = Tempfile.open("content")
         tf.write(response.body)
       end
     end
-  end
+  }
 
-  "OK"
 end
