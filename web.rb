28b2b71bcf3123fa1f5ce4f25afeb000e8d9ebdd
chunkaihuang <atohuang@bu.edu>
2017-05-22 16:11:12 +0800

    Update web.rb

--- web.rb
+++ web.rb
@@ -23,10 +23,7 @@ post '/callback' do
     when Line::Bot::Event::Message
       case event.type
       when Line::Bot::Event::MessageType::Text
-        message = {
-          type: 'text',
-          text: event.message['text']
-        }
+        message = "你好我是黃肯尼"
         client.reply_message(event['replyToken'], message)
       end
     end
